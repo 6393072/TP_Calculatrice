48610551b8d0c331e6252957e19aa396e07f205e
Samantha Villanueva <6393072@bdeb.qc.ca>
2026-02-24 15:33:20 -0500

    first commit

---
+++ src/calculatrice.py
@@ -0,0 +1,13 @@
+def addition(a,b):
+    return a+b
+
+def soustraccion(a,b):
+    return a-b
+
+def multiplication(a,b):
+    return a*b
+
+def division(a,b):
+    if b == 0:
+        raise ValueError("Division par zéro interdite.")
+    return a/b
